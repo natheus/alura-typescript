4eb61a17a5e88fe2ba754553b4f8a079a7ddaeac
Natheus <natheusmunes@gmail.com>
2022-04-18 14:11:31 -0300

    decorators de métodos

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,8 +1,8 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
   protected elemento: HTMLElement;
-  private escapar = false;
 
   constructor(seletor: string, escapar?: boolean) {
     const elemento = document.querySelector(seletor);
@@ -11,17 +11,12 @@ export abstract class View<T> {
     } else {
       throw Error(`Seletor ${seletor} não existe no Dom. Verifique.`);
     }
-    if (escapar) {
-      this.escapar = escapar;
-    }
   }
 
-  @logarTempoDeExecucao()
+  @logarTempoDeExecucao(true)
+  @inspect()
   public update(model: T): void {
     let template = this.template(model);
-    if (this.escapar) {
-      template = template.replace(/<script>?[\s\S]*?<\/script>/, "");
-    }
     this.elemento.innerHTML = template;
   }
 
